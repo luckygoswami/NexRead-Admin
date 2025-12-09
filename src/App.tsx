30294b7033dfcda4177fd1df09743ee83c592317
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-12-09 08:51:10 +0530

    feat: initialize React application with TypeScript, Tailwind CSS, and routing
    - Created main application component (App.tsx) with a placeholder title.
    - Added global styles using Tailwind CSS and custom CSS variables (index.css).
    - Implemented utility function for class name merging (utils.ts).
    - Set up main entry point for the application (main.tsx) with React Router.
    - Defined basic routing structure with a home page (router.tsx).
    - Configured TypeScript settings for the application (tsconfig.app.json, tsconfig.node.json, tsconfig.json).
    - Set up Vite configuration for the project (vite.config.ts) with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <>NexRead - Admin</>;
+}
+
+export default App;
---
+++ src/router.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Home Page</div>,
+  },
+]);
+
+export default router;
