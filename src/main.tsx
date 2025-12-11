391f700829f70a4adff2ec88bf9a91b8bbe0664a
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-12-11 19:43:38 +0530

    feat: add axios and tanstack react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,14 @@ import { RouterProvider } from 'react-router';
 import router from './router';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
